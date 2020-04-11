783534fa172342e27318c26df1b2b584b8795bfc
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-04-12 00:45:29 +0300

    .
    .

--- test.py
+++ test.py
@@ -4,26 +4,30 @@ import pickle
 import joblib
 
 # Verinin Okunması #s
-column = ['MansetBaslık']
-df = pd.read_csv('HeadLines.csv',encoding = 'utf-8',sep = '"')
+column = ['Cümle']
+df = pd.read_csv('SmartClassRoom.csv',encoding = 'utf-8',sep = '"')
 df.columns = column
 df.info()
 #Veri setindeki Türkçe Dolgu kelimlerinin kaldırılması 
 def remove_stopwords(df_fon):
     stopwords = open('turkce-stop-words','r').read().split()
     df_fon['stopwords_removed'] = list(map(lambda doc:
-        [word for word in doc if word not in stopwords],df_fon['MansetBaslık']))
+        [word for word in doc if word not in stopwords],df_fon['yorum']))
         
-# 1 Ekonomi 2 Sağlık
-df['Manset'] = 1
-df.Manset.iloc[10:21] = 2
+# 1 Fan Açık 2 Fan Kapalı 3 Işık Açık 4 Işık Kapalı
+df['Sinif'] = 1
+df.Sinif.iloc[5:10] = 2
+df.Sinif.iloc[10:15] = 3
+df.Sinif.iloc[15:21] = 4
+
 from sklearn.model_selection import train_test_split
-X_train,X_test,Y_train,Y_test = train_test_split(df['MansetBaslık'],df['Manset'],test_size = 0.6,random_state = 0)
+X_train,X_test,Y_train,Y_test = train_test_split(df['Cümle'],df['Sinif'],test_size = 0.3,random_state = 0)
 
 from sklearn.feature_extraction.text import CountVectorizer
 vect = CountVectorizer(encoding ='iso-8859-9').fit(X_train)
 feature_list = vect.get_feature_names()
 joblib.dump(feature_list, 'vocabulary.pkl')
+#X_Train'deki belgeleri bir belge terim matrisine dönüştürüyoruz
 X_train_vectorizer = vect.transform(X_train)
 
 from sklearn.linear_model import LogisticRegression
@@ -36,12 +40,9 @@ model = pickle.load(open('model.pkl', 'rb'))
 from sklearn.metrics import roc_auc_score
 predictions = model.predict(vect.transform(X_test))
 
-from sklearn.metrics import roc_auc_score
-roc_auc_score(Y_test, predictions)
-print(roc_auc_score(Y_test, predictions))
-#print('"0,8333" AUC: ', roc_auc_score(Y_test, predictions))
-
-
-
+from sklearn.metrics import zero_one_loss
+zero_one_loss(Y_test, predictions)
+print(zero_one_loss(Y_test, predictions))
+#print('"0,745" AUC: ', roc_auc_score(Y_test, predictions))
 
 
