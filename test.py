c99c22c7a51b1a5c2b6c7d6a3d1425ee5cb6a3e5
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-04-11 17:42:24 +0300

    .
    .

--- test.py
+++ test.py
@@ -5,23 +5,23 @@ import joblib
 
 # Verinin Okunması #s
 column = ['Cümle']
-df = pd.read_csv('TouristinInfo.csv',encoding = 'utf-8',sep = '"')
+df = pd.read_csv('SmartClassRoom.csv',encoding = 'utf-8',sep = '"')
 df.columns = column
 df.info()
-s
 #Veri setindeki Türkçe Dolgu kelimlerinin kaldırılması 
 def remove_stopwords(df_fon):
     stopwords = open('turkce-stop-words','r').read().split()
     df_fon['stopwords_removed'] = list(map(lambda doc:
         [word for word in doc if word not in stopwords],df_fon['yorum']))
         
-# 1 Müze 2 Balıklar 3 Araba
-df['Info'] = 1
-df.Info.iloc[5:10] = 2
-df.Info.iloc[10:16] = 3
+# 1 Fan Açık 2 Fan Kapalı 3 Işık Açık 4 Işık Kapalı
+df['Sinif'] = 1
+df.Sinif.iloc[5:10] = 2
+df.Sinif.iloc[10:15] = 3
+df.Sinif.iloc[15:21] = 4
 
 from sklearn.model_selection import train_test_split
-X_train,X_test,Y_train,Y_test = train_test_split(df['Cümle'],df['Info'],test_size = 0.4,random_state = 0)
+X_train,X_test,Y_train,Y_test = train_test_split(df['Cümle'],df['Sinif'],test_size = 0.3,random_state = 0)
 
 from sklearn.feature_extraction.text import CountVectorizer
 vect = CountVectorizer(encoding ='iso-8859-9').fit(X_train)
@@ -40,8 +40,9 @@ model = pickle.load(open('model.pkl', 'rb'))
 from sklearn.metrics import roc_auc_score
 predictions = model.predict(vect.transform(X_test))
 
-from sklearn.metrics import recall_score
-recall_score(Y_test, predictions, average='macro')
-print(recall_score(Y_test, predictions, average='macro'))
+from sklearn.metrics import zero_one_loss
+zero_one_loss(Y_test, predictions)
+print(zero_one_loss(Y_test, predictions))
 #print('"0,745" AUC: ', roc_auc_score(Y_test, predictions))
+
 
